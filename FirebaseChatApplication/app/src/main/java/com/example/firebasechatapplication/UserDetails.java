6a025a0b714beefbb3f99ddb2fe13d41bc19e8db
niraj-web <saraswatniraj99@gmail.com>
2021-03-15 13:05:39 +0530

    Android

---
+++ FirebaseChatApplication/app/src/main/java/com/example/firebasechatapplication/UserDetails.java
@@ -0,0 +1,12 @@
+package com.example.firebasechatapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserDetails
+{
+    static String username = "";
+    static String password = "";
+    static String chatWith = "";
+}
